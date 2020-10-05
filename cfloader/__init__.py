d13024daeadc2f6a15c4e5eefbb0888f106b40d1
Shachi Bista <shachi@smsgrupp.se>
2020-10-06 00:58:01 +0200

    Bump version: 0.1.2 → 0.1.3

--- cfloader/__init__.py
+++ cfloader/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Shachi Bista"""
 __email__ = "shachibista@gmail.com"
-__version__ = "__version__ = '0.1.2'"
+__version__ = "__version__ = '0.1.3'"
 
 from cfloader.fs import open
 from cfloader.readers import Path, Archive
